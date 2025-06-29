1dc97bd462f5377c4d3c9d04c9e7781ea0bed76f
Max <mm.tito@yavirac.edu.ec>
2025-06-29 16:55:24 -0500

    100 complete

--- administrador/admin.php
+++ administrador/admin.php
@@ -8,13 +8,72 @@ if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'admin') {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8" />
     <title>Panel Administrador</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" />
+
 </head>
+
 <body>
+
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false"><?php echo htmlspecialchars($_SESSION['usuario']); ?></a>
+                <ul class="dropdown-menu">
+            </li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="../auth/logout.php">Cerrar session</a></li>
+        </ul>
+        </li>
+        </ul>
+    </nav>
+
+
+
+
     <h1>Bienvenido Admin: <?php echo htmlspecialchars($_SESSION['usuario']); ?></h1>
-    <p>Esta es la vista del administrador.</p>
-    <a href="../auth/logout.php">Cerrar sesión</a>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">cliente</th>
+                <th scope="col">producto</th>
+                <th scope="col">factura</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>John</td>
+                <td>Doe</td>
+                <td>@social</td>
+            </tr>
+        </tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
--- cliente/cliente.php
+++ cliente/cliente.php
@@ -8,17 +8,98 @@ if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'cliente') {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8" />
-    <title>Panel Cliente</title>
+    <title><?php echo htmlspecialchars($_SESSION['usuario']); ?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" />
+
 </head>
+
 <body>
-    <h1>Bienvenido <?php echo htmlspecialchars($_SESSION['nombre']); 
-        ?>: 
-        <?php echo htmlspecialchars($_SESSION['usuario']); 
+    <nav class="navbar bg-primary border-bottom border-body" data-bs-theme="dark">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                    aria-expanded="false"><?php echo htmlspecialchars($_SESSION['usuario']); ?></a>
+                <ul class="dropdown-menu">
+            </li>
+            <li>
+                <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="../auth/logout.php">Cerrar session</a></li>
+        </ul>
+        </li>
+        </ul>
+    </nav>
+
+    <h1>Bienvenido <?php echo htmlspecialchars($_SESSION['nombre']);
+    ?>:
+        <?php echo htmlspecialchars($_SESSION['usuario']);
         ?>
     </h1>
-    <p>Esta es la vista del cliente.</p>
-    <a href="../auth/logout.php">Cerrar sesión</a>
+    <div class="card-group">
+        <div class="card">
+            <img src="/secureweb/cliente/aut.jpeg" class="card-img-top" alt="/secureweb/cliente/aut.jpeg">
+            <div class="card-body">
+                <h5 class="card-title">Producto #</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/secureweb/cliente/aut.jpeg" class="card-img-top" alt="/secureweb/cliente/aut.jpeg">
+            <div class="card-body">
+                <h5 class="card-title">Producto #</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/secureweb/cliente/aut.jpeg" class="card-img-top" alt="/secureweb/cliente/aut.jpeg">
+            <div class="card-body">
+                <h5 class="card-title">Producto #</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+    </div>
+    <div class="card-group">
+        <div class="card">
+            <img src="/secureweb/cliente/aut.jpeg" class="card-img-top" alt="/secureweb/cliente/aut.jpeg">
+            <div class="card-body">
+                <h5 class="card-title">Producto #</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/secureweb/cliente/aut.jpeg" class="card-img-top" alt="/secureweb/cliente/aut.jpeg">
+            <div class="card-body">
+                <h5 class="card-title">Producto #</h5>
+                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+        <div class="card">
+            <img src="/secureweb/cliente/aut.jpeg" class="card-img-top" alt="/secureweb/cliente/aut.jpeg">
+            <div class="card-body">
+                <h5 class="card-title">Producto #</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This card has even longer content than the first to show that equal height action.</p>
+                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            </div>
+        </div>  
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>

c624fb7ed6051e04b441a37fa84f659178767c4b
Max <mm.tito@yavirac.edu.ec>
2025-06-29 21:14:44 -0500

    100 complete

--- db/usuario.sql
+++ db/usuario.sql
@@ -1,3 +1,13 @@
+CREATE DATABASE secureweb;
+USE secureweb;
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    usuario VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    rol ENUM('admin', 'cliente') NOT NULL
+);
+
 INSERT INTO usuarios (nombre, usuario, password, rol)
 VALUES 
 ('Admin', 'alejandra', SHA2('alejandra', 256), 'admin'),
